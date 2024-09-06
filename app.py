a139499a1f109f34f0d5be34723d42fb128fa863
wenjiun1024 <wenjiun@yahoo.com>
2024-09-06 10:54:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 app = Dash(__name__)
 server = app.server
 
-app.layout = [html.Div(children='Hello World')]
+app.layout = [html.Div(children='Hello MMU!')]
 
 if __name__ == '__main__':
     app.run(debug=True)
