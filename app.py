f5f243cf15f67b966126e5007b52440007f5689f
wenjiun1024 <wenjiun@yahoo.com>
2024-09-09 12:00:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.layout = [html.H1('MCM7183 Exercise 3'),
               html.Img(src=image_path), 
               dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='country_selected'), 
               dcc.Graph(id="graph_scatter"), 
-              dcc.Dropdown([{"label": "2020", "value": 2020},{"label": "2010", "value": 2010}, {"label": "2000", "value": 2000}], 2020, id='year_selected'), 
+              dcc.Slider(1960, 2020, 1, value=2020, id='year_selected'),
               dcc.Graph(id="graph_pie")]
 
 @callback(
