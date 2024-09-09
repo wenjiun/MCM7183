34fc6c02d14909a20ad8c250ea6bbebbe3972eac
wenjiun1024 <wenjiun@yahoo.com>
2024-09-09 11:40:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,31 +9,36 @@ server = app.server
 
 df = pd.read_csv("https://raw.githubusercontent.com/wenjiun/MCM7183Exercise3/main/assets/gdp_1960_2020.csv")
 
-subset_2020 = df[df['year'].isin([2020])]
-subset_2020_Asia = subset_2020[subset_2020['state'].isin(["Asia"])]
-subset_2020_Africa = subset_2020[subset_2020['state'].isin(["Africa"])]
-subset_2020_America = subset_2020[subset_2020['state'].isin(["America"])]
-subset_2020_Europe = subset_2020[subset_2020['state'].isin(["Europe"])]
-subset_2020_Oceania = subset_2020[subset_2020['state'].isin(["Oceania"])]
-pie_data = [sum(subset_2020_Asia['gdp']),sum(subset_2020_Africa['gdp']),sum(subset_2020_America['gdp']),sum(subset_2020_Europe['gdp']),sum(subset_2020_Oceania['gdp'])];
-mylabels = ["Asia", "Africa", "America", "Europe","Oceania"]
-pie_df = {'Continent': mylabels,'GDP': pie_data}
-fig2 = px.pie(pie_df,values="GDP",names="Continent")
-
 image_path = 'assets/logo-mmu.png'
 
-app.layout = [html.H1('MCM7183 Exercise 3'), html.Img(src=image_path), 
-              dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='country_selected'),
-              dcc.Graph(id="figure_gdp"), dcc.Graph(figure=fig2)]
+app.layout = [html.H1('MCM7183 Exercise 3'), 
+              html.Img(src=image_path), 
+              dcc.Dropdown(['Malaysia', 'Indonesia', 'China'], 'Malaysia', id='country_selected'), 
+              dcc.Graph(id="graph_scatter"), dcc.Dropdown(['2020', '2010', '2000'], '2020', id='year_selected'), 
+              dcc.Graph(id="graph_pie")]
 
 @callback(
-    Output('figure_gdp', 'figure'),
-    Input('country_selected', 'value')
+    Output('graph_scatter', 'figure'),
+    Output('graph_pie', 'figure'),
+    Input('country_selected', 'value'),
+    Input('year_selected', 'value')
 )
-def update_figure(value):
-    subset_Country = df[df['country'].isin([value])]
+def update_figure(country, year):
+    subset_Country = df[df['country'].isin([country])]
     fig = px.scatter(subset_Country, x="year", y="gdp")
-    return fig
+
+    subset_year = df[df['year'].isin([year])]
+    subset_year_Asia = subset_year[subset_year['state'].isin(["Asia"])]
+    subset_year_Africa = subset_year[subset_year['state'].isin(["Africa"])]
+    subset_year_America = subset_year[subset_year['state'].isin(["America"])]
+    subset_year_Europe = subset_year[subset_year['state'].isin(["Europe"])]
+    subset_year_Oceania = subset_year[subset_year['state'].isin(["Oceania"])]
+    pie_data = [sum(subset_year_Asia['gdp']),sum(subset_year_Africa['gdp']),sum(subset_year_America['gdp']),sum(subset_year_Europe['gdp']),sum(subset_year_Oceania['gdp'])];
+    mylabels = ["Asia", "Africa", "America", "Europe","Oceania"]
+    pie_df = {'Continent': mylabels,'GDP': pie_data}
+    fig2 = px.pie(pie_df,values="GDP",names="Continent")
+
+    return fig, fig2
 
 if __name__ == '__main__':
     app.run(debug=True)
